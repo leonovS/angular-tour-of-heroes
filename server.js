17ca0eda30abdd4280aba4c7c06876c1654ad886
LeonovS <leon19840709@gmail.com>
2022-02-17 20:51:32 +0300

    3.2

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(`${__dirname}\dist\angular - tour - of - heroes`));
+app.use(express.static(`${__dirname}/dist/angular - tour - of - heroes`));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}\dist\angular - tour - of - heroesindex.html`));
+  res.sendFile(path.join(`${__dirname}/dist/angular - tour - of - heroesindex.html`));
 });
 
 app.listen(process.env.PORT || 3000);
